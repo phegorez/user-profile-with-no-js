8e10c2113c3282750de7f8568be65ce15ba50ca6
phegorez <metha.nompd@gmail.com>
2023-01-13 11:51:11 +0700

    save

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//Include html elements
 const idElement = document.getElementById('profile-id')
 const nameElement = document.getElementById('profile-name')
 const emailElement = document.getElementById('profile-email')
@@ -5,34 +6,32 @@ const avatarElement = document.getElementById('profile-avatar')
 const addressElement = document.getElementById('profile-address')
 const companyBsElement = document.getElementById('profile-company-bs')
 const companyNameElement = document.getElementById('profile-company-name')
+//
 
 
-const updateProfile = (profile, avatarnumber) => {
-
-
-
+//Display function
+const display = (profile, index) => {
     idElement.innerText = profile.id
     nameElement.innerText = profile.name
     emailElement.innerText = profile.email
-    avatarElement.src = `https://api.dicebear.com/5.x/adventurer/svg?seed=${avatarnumber}`
-    addressElement.innerText = `${profile.address.street}, ${profile.address.suite}, ${profile.address.city}, ${profile.address.zipcode}`
-    companyNameElement.innerText = profile.company.name
-    companyBsElement.innerText = profile.company.bs
+    avatarElement.src = `https://api.dicebear.com/5.x/adventurer/svg?seed=${index}`
+    addressElement.innerText = `${profile.address.street}, ${profile.address.suite}, ${profile.address.city}, ${profile.address.state}, ${profile.address.zipcode}`
+    companyNameElement.innerText = `${profile.company.name}, ${profile.company.catchPhrase}`
+    companyBsElement.innerText = `${profile.company.bs}`
 }
 
-const fetchEmployee = async (number) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${number}`)
-
+const fetchEmployee = async (index) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${index}`)
     return await res.json()
 }
 
 const run = async () => {
 
-    const  random = Math.floor(Math.random() * 10 + 1)
+    const randomIndex = Math.floor(Math.random() * 10 + 1)
+    
+    const profile = await fetchEmployee(randomIndex)
 
-    const profile = await fetchEmployee(random)
-    updateProfile(profile, random)
+    display(profile, randomIndex)
 }
 
 run()
-
